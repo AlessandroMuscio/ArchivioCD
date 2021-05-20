b173c96314b20fa35285f4e6d7b58be63ae290fb
AlessandroMuscio <muscioalex30@gmail.com>
2021-05-20 12:01:19 +0200

    Completata classe archivioCD

--- src/ArchivioCD.java
+++ src/ArchivioCD.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 
+import it.unibs.fp.mylib.EstrazioniCasuali;
+
 public class ArchivioCD {
   private ArrayList<CD> lista_cd;
 
@@ -18,6 +20,43 @@ public class ArchivioCD {
   public void setLista_cd(ArrayList<CD> lista_cd) {
     this.lista_cd = lista_cd;
   }
+
+  public boolean aggiungiCD(CD cd) {
+    return lista_cd.add(cd);
+  }
+
+  public CD cercaCDperTitolo(String titolo) {
+    for (CD cd : lista_cd) {
+      if (titolo.equals(cd.getTitolo()))
+        return cd;
+    }
+
+    return null;
+  }
+
+  public void visualizzaCD(String titolo) {
+    CD cd_da_visualizzare = cercaCDperTitolo(titolo);
+
+    if (cd_da_visualizzare == null)
+      System.out.println(String.format("Impossibile trovare un cd con titolo \"%s\"", titolo));
+    else
+      System.out.println(cd_da_visualizzare);
+  }
+
+  public boolean eliminaCD (String titolo) {
+    CD cd_da_eliminare = cercaCDperTitolo(titolo);
+    
+    if (cd_da_eliminare == null) {
+      System.out.println(String.format("Impossibile trovare un cd con titolo \"%s\"", titolo));
+      return false;
+    } else {
+      return lista_cd.remove(cd_da_eliminare);
+    }
+  }
+
+  public CD getCDCasuale() {
+    return lista_cd.get(EstrazioniCasuali.estraiIntero(0, (lista_cd.size()-1)));
+  }  
 
   @Override
   public String toString() {
